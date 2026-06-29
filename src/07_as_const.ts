d1b20fa8cdca7a57e6f3ea7465e8202d7c2493e4
dicky375 <danieldickson601@gmail.com>
2026-06-29 14:55:10 +0100

    this is about  deriving an aray as a unison value

---
+++ src/07_as_const.ts
@@ -0,0 +1,13 @@
+
+
+
+const ROLES = ["admin", "user", "operator"] as const
+//derive a union from the array
+type Role = (typeof ROLES) [number]
+
+function setRole(r: Role){
+    console.log(r)
+
+}
+
+setRole('user');
