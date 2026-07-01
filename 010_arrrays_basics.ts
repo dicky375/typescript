2871af8eefe89c644d51e5ded1f8f1b735eeb589
dicky375 <danieldickson601@gmail.com>
2026-07-01 09:20:53 +0100

    understood basic  arrays in ts

---
+++ 010_arrrays_basics.ts
@@ -0,0 +1,9 @@
+
+
+const all: number[] = [1,2,3] // T[]
+const a22: Array<number> = [1,2,3]//Array<T>
+
+const scores = [10,20,30]
+//scores.push('40')
+
+const mix = [1,2,3,'4'] // (string | number)[]
