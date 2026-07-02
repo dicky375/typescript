e17c48afe79c88a72b9ed71eabfe23391d0a4b9c
dicky375 <danieldickson601@gmail.com>
2026-07-03 00:40:13 +0100

    utils awaited

---
+++ src/029_util_awaited.ts
@@ -0,0 +1,28 @@
+
+
+ 
+//nested Promise<Promise<T>>
+//Thenables ->
+
+//Awaited<T>
+
+
+type Promise1 = Awaited<Promise<number>>// number
+type Promise2 = Awaited<Promise<string>>
+type Awaited1 = Awaited<string> //string
+
+type PromiseUnionExample = Awaited<Promise<string | number>>
+
+async function fetchCount(){
+    return 42 as const
+}
+
+type ResolvedFetchCountValue = Awaited<ReturnType<typeof fetchCount>>
+async function getData(){
+    return Promise.all([
+        Promise.resolve(1 as const),
+        Promise.resolve("x" as const)
+    ] as const)
+}
+
+type DataTupleWithPromise = Awaited<ReturnType<typeof getData>> // [1, "x"] readonly
