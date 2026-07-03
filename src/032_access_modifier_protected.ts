e312ea3c9fbbf6a050b2cd637d232d252510cf45
dicky375 <danieldickson601@gmail.com>
2026-07-03 01:13:40 +0100

    access modifiers protected

---
+++ src/032_access_modifier_protected.ts
@@ -0,0 +1,26 @@
+
+
+//Protected -> class, subclasses
+
+class Animal {
+    protected energy = 23
+
+    eat(amount: number){
+        this.energy = Math.min(100, this.energy + amount)
+    }
+}
+
+class Dog extends Animal{
+    run(){
+        this.energy -= 10
+
+    }
+    status(){
+        return this.energy
+    }
+}
+
+const d = new Dog()
+d.eat(10)
+d.run()
+d.status()
